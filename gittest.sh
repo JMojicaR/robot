6bed914268ee6a2c905e893c130f72c23b6d497a
JMojicaR <jrodmoj@gmail.com>
2022-02-09 15:44:37 -0600

    prueba1

---
+++ gittest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GIT_USERNAME='JMojicaR'
+GIT_EMAIL='jrodmoj@gmail.com'
+GIT_BRANCH='main_test2'
+
+
+git config --global user.name "${GIT_USERNAME}"
+git config --global user.email "${GIT_EMAIL}"
+git checkout master
+git pull
+git checkout -b $GIT_BRANCH
+git add .
+git commit -m "prueba uno"
+git push origin $GIT_BRANCH
