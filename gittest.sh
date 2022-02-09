4f6b1b5507be97295d0eda6e3fd1df6fab7b003a
JMojicaR <jrodmoj@gmail.com>
2022-02-09 17:01:38 -0600

    prueba2

--- gittest.sh
+++ gittest.sh
@@ -3,6 +3,7 @@
 GIT_USERNAME='JMojicaR'
 GIT_EMAIL='jrodmoj@gmail.com'
 GIT_BRANCH='main_test2'
+GIT_BRANCH2MERGE='mybranch2'
 
 
 git config --global user.name "${GIT_USERNAME}"
@@ -13,3 +14,4 @@ git checkout -b $GIT_BRANCH
 git add .
 git commit -m "prueba uno"
 git push origin $GIT_BRANCH
+git merge $GIT_BRANCH2MERGE
